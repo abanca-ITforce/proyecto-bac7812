c9779b935faaf879c47940e35cee1ef4a2a39c6c
Usuario <bac7812@gmail.com>
2019-12-03 18:30:10 +0100

    new material

--- explorer/src/app/layout/layout.module.ts
+++ explorer/src/app/layout/layout.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
+import { LayoutModule as MaterialLayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavShellComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [NavShellComponent]
 })
 export class LayoutModule { }
